606672488fde87622152672589f4aa7ff8cb12bd
J. David Mendoza <jdmendoza@um.edu.mx>
2012-04-09 09:12:36 -0500

    Agregamos Spring 3.1

---
+++ src/main/java/mx/edu/um/escuela/Alumno.java
@@ -0,0 +1,75 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2012 Universidad de Montemorelos A. C.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package mx.edu.um.escuela;
+
+/**
+ *
+ * @author J. David Mendoza <jdmendoza@um.edu.mx>
+ */
+public class Alumno {
+    private String nombre;
+    private String apellido;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+    
+}
--- src/main/java/mx/edu/um/escuela/App.java
+++ src/main/java/mx/edu/um/escuela/App.java
@@ -1,5 +1,9 @@
 package mx.edu.um.escuela;
 
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -7,6 +11,13 @@ package mx.edu.um.escuela;
 public class App {
 
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("escuela.xml");
+        AlumnoDao alumnoDao = (AlumnoDao) context.getBean("alumnoDao");
+        List<Alumno> alumnos = alumnoDao.lista();
+        for(Alumno alumno : alumnos) {
+            System.out.println(alumno);
+        }
+        
         App app = new App();
         System.out.println(app.mensaje());
     }
