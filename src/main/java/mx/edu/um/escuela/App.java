c2de9be72f99f16a53811423253631b90c30d833
J. David Mendoza <jdmendoza@um.edu.mx>
2012-04-09 11:38:18 -0500

    Aplicacion en consola con alumnos

--- src/main/java/mx/edu/um/escuela/App.java
+++ src/main/java/mx/edu/um/escuela/App.java
@@ -1,6 +1,6 @@
 package mx.edu.um.escuela;
 
-import java.util.List;
+import java.util.Scanner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,9 +13,47 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("escuela.xml");
         AlumnoDao alumnoDao = (AlumnoDao) context.getBean("alumnoDao");
-        List<Alumno> alumnos = alumnoDao.lista();
-        for(Alumno alumno : alumnos) {
-            System.out.println(alumno);
+        
+        System.out.println("Menu:");
+        System.out.println("1. Lista de Alumnos");
+        System.out.println("2. Crea alumno");
+        System.out.println("3. Obtiene alumno");
+        System.out.println("X. Salir");
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            Alumno alumno;
+            String line = scanner.nextLine();
+            switch(line) {
+                case "1" : 
+                    for(Alumno a : alumnoDao.lista()) {
+                        System.out.println(a);
+                    }
+                    break;
+                case "2" :
+                    alumno = new Alumno();
+                    System.out.println("Matricula:");
+                    alumno.setMatricula(scanner.nextLine());
+                    System.out.println("Nombre:");
+                    alumno.setNombre(scanner.nextLine());
+                    System.out.println("Apellido:");
+                    alumno.setApellido(scanner.nextLine());
+                    alumnoDao.crea(alumno);
+                    System.out.println("Alumno CREADO!!!");
+                    break;
+                case "3":
+                    System.out.println("Matricula:");
+                    alumno = alumnoDao.obtiene(scanner.nextLine());
+                    System.out.println(alumno);
+                    break;
+                case "X":
+                    System.out.println("Adios...");
+                    System.exit(0);
+            }
+            System.out.println("Menu:");
+            System.out.println("1. Lista de Alumnos");
+            System.out.println("2. Crea alumno");
+            System.out.println("3. Obtiene alumno");
+            System.out.println("X. Salir");
         }
         
         App app = new App();
