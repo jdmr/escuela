b843bcc22fd211811966386f5bf505129afd5d2f
J. David Mendoza <jdmendoza@um.edu.mx>
2012-04-16 11:47:20 -0500

    Activando cache en hibernate

--- src/test/java/mx/edu/um/escuela/AlumnoDaoTest.java
+++ src/test/java/mx/edu/um/escuela/AlumnoDaoTest.java
@@ -31,7 +31,6 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
